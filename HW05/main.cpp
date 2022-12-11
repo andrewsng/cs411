52be4346ed3a35cb29b05a768b2b607b3e11021d
Andrew Ng <ngandrew1337@gmail.com>
2022-12-11 04:37:49 -0900

    Generate combinations of integers

--- HW05/main.cpp
+++ HW05/main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
+using std::cout;
+#include <vector>
+using std::vector;
 
 
 int main()
 {
-    std::cout << "Hello, World!\n";
+    size_t deckSize = 5;
+    vector<int> cards(deckSize);
+    std::cout << cards.size() << "\n";
+    for (size_t i = 0; i < cards.size(); ++i)
+    {
+        cards[i] = i + 1;
+    }
+
+    vector<vector<int>> combinations{};
+    size_t handSize = 3;
+    vector<int> comb(handSize);
+    for (size_t min1 = 0; min1 < deckSize; ++min1)
+    {
+        comb[0] = cards[min1];
+        for (size_t min2 = min1 + 1; min2 < deckSize; ++min2)
+        {
+            comb[1] = cards[min2];
+            for (size_t min3 = min2 + 1; min3 < deckSize; ++min3)
+            {
+                comb[2] = cards[min3];
+                combinations.push_back(comb);
+            }
+        }
+    }
+    for (const auto &comb : combinations)
+    {
+        for (const auto &card : comb)
+        {
+            std::cout << card;
+        }
+        std::cout << "\n";
+    }
+    std::cout << std::endl;
 }
